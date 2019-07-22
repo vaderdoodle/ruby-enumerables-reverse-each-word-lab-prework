c388fdf3bae11e4cad8c02d2a0e7841b20aeb79b
Hemesh Patel <hemesh.v.patel@gmail.com>
2019-07-22 23:15:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ end
 
 def reverse_each_word(string)
   new_array = string.split
-  reversed_array = new_array.each {|word| word.reverse!}
+  reversed_array = new_array.collect {|word| word.reverse!}
   return reversed_array.join(" ")
 end
